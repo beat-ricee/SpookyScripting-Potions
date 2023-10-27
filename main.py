463920a558cb72321208ecce95e8e8f378e59817
beat-ricee <144713892+beat-ricee@users.noreply.github.com>
2023-10-27 17:09:41 +0000

    changes

--- main.py
+++ main.py
@@ -10,24 +10,72 @@ potiong = Actor("potiong")
 potiong.center = WIDTH / 2, HEIGHT / 2
 wand = Actor("wand")
 wand.topright = 0, 10
+purpleGIF = [
+    "purple(1)",
+    "purple(2)",
+    "purple(3)",
+    "purple(4)",
+    "purple(5)",
+    "purple(6)",
+    "purple(7)",
+    "purple(8)",
+    "purple(9)",
+    "purple(10)",
+    ]
+purple_frame = 0
+greenGIF = [
+    "greens/green1",
+    "greens/green2",
+    "greens/green3",
+    "greens/green4",
+    "greens/green5",
+    "greens/green6",
+    "greens/green7",
+    "greens/green8",
+    "greens/green9",
+    "greens/green10",
+]
+green_frame = 0
+potion_color = "static"
+pumpkin_start = Actor("pumpkin_start")
+pumpkin_start.bottomright = WIDTH, HEIGHT
 
 
 def draw():
     screen.fill((196, 164, 132))
     wand.draw()
     potiong.draw()
+    pumpkin_start.draw()
 
 
 def update():
+    global green_frame
+    global purple_frame
     wand.left += 2
     if wand.left > WIDTH:
         wand.right = 0
+    if potion_color == "static":
+        potiong.image = "potiong"
+    elif potion_color == "green":
+        potiong.image = greenGIF[green_frame // 5]
+        green_frame += 1
+        green_frame %= len(greenGIF) * 5
+    elif potion_color == "green2purple":
+        potiong.image = green2purpleGIF[green2purple_frame // 5]
+        green2purple_frame += 1
+        green2purple_frame = len(green2purpleGIF):
+        potion_color == "purple"
+    elif potion_color == "purple":
+        pass
 
 
 def on_mouse_down(pos):
+    global potion_color
     if wand.collidepoint(pos):
-        GreenGIF.draw()
-        
+        potion_color = "green"
+    if pumpkin_start.collidepoint(pos):
+        # screen.fill
+
         pass
         # sounds.shuffle_cards.play()
         # wand.center
